610bb66db717f8be6a0d135b16539aa2f6eb36ae
Léo <leo.maisonneuve@gmail.com>
2024-06-03 15:10:26 +0200

    Fix error when no campaign

--- trunk/admin/class-hello-asso-admin.php
+++ trunk/admin/class-hello-asso-admin.php
@@ -20,16 +20,14 @@
  * @subpackage Hello_Asso/admin
  * @author     HelloAsso
  */
-
-class Hello_Asso_Admin
-{
+class Hello_Asso_Admin {
 
 	/**
 	 * The ID of this plugin.
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
+	 * @var      string $plugin_name The ID of this plugin.
 	 */
 	private $plugin_name;
 
@@ -38,19 +36,18 @@ class Hello_Asso_Admin
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
+	 * @var      string $version The current version of this plugin.
 	 */
 	private $version;
 
 	/**
 	 * Initialize the class and set its properties.
 	 *
+	 * @param string $plugin_name The name of this plugin.
+	 * @param string $version The version of this plugin.
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
 	 */
-	public function __construct($plugin_name, $version)
-	{
+	public function __construct($plugin_name, $version) {
 
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
@@ -61,8 +58,7 @@ class Hello_Asso_Admin
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_styles()
-	{
+	public function enqueue_styles() {
 
 		/**
 		 * This function is provided for demonstration purposes only.
@@ -79,8 +75,7 @@ class Hello_Asso_Admin
 		wp_enqueue_style($this->plugin_name, esc_url(plugin_dir_url(__FILE__)) . 'css/hello-asso-admin.css', array(), $this->version, 'all');
 	}
 
-	public function add_menu()
-	{
+	public function add_menu() {
 		$urlIcon = esc_url(plugin_dir_url(__FILE__)) . 'img/icon-28x28.svg';
 		add_menu_page('HelloAsso', 'HelloAsso', 'manage_options', 'hello-asso', 'content_dashboard', $urlIcon, 10);
 
@@ -93,16 +88,16 @@ class Hello_Asso_Admin
 			'content_dashboard' //callback function
 		);
 
-		function content_dashboard()
-		{
+		function content_dashboard() {
 			require('view/dashboard.php');
 		}
+
 		if (get_option('ha-slug') != '') {
 			$campaign = get_option('ha-campaign');
 			$nbCampaign = 0;
 
 			foreach ($campaign as $key => $campain) :
-			
+
 				if (isset($campain['endDate']) && strlen($campain['endDate']) > 4) {
 					if (time() > strtotime($campain['endDate'])) {
 						$incrementArray = 0;
@@ -114,7 +109,7 @@ class Hello_Asso_Admin
 				}
 
 				if (strtolower($campain['formType']) == "event") {
-					
+
 					if (isset($campain['startDate']) && time() > strtotime($campain['startDate'])) {
 						$incrementArray = 0;
 					} else {
@@ -241,27 +236,22 @@ class Hello_Asso_Admin
 			);
 		}
 
-		function content_campaigns()
-		{
+		function content_campaigns() {
 			require('view/campaign.php');
 		}
 
-		function error_1()
-		{
+		function error_1() {
 			require('view/error_1.php');
 		}
 
-		function error_2()
-		{
+		function error_2() {
 			require('view/error_2.php');
 		}
 	}
 
-	public function add_to_gutenberg()
-	{
+	public function add_to_gutenberg() {
 
-		function loadGutenbergBlock()
-		{
+		function loadGutenbergBlock() {
 			wp_enqueue_script(
 				'ha-gutenberg',
 				esc_url(plugin_dir_url(__FILE__)) . 'js/ha-gutenberg.js',
@@ -274,10 +264,8 @@ class Hello_Asso_Admin
 	}
 
 
-	public function add_button_tinymce()
-	{
-		function wporg_add_custom_box()
-		{
+	public function add_button_tinymce() {
+		function wporg_add_custom_box() {
 			$screens = ['post', 'page'];
 			foreach ($screens as $screen) {
 				add_meta_box(
@@ -288,29 +276,29 @@ class Hello_Asso_Admin
 				);
 			}
 		}
+
 		add_action('add_meta_boxes', 'wporg_add_custom_box');
 
-		function wporg_custom_box_html($post)
-		{
-?>
-			<div id="ha-popup" class="ha-overlay">
-				<div class="ha-popup">
-					<main>
-						<a class="close" href="#">&times;</a>
-						<button type="button" class="ha-btn ha-btn-secondary ha-return" style="display: none;" onclick="haReturn()">
-							<img src="<?= esc_url(plugin_dir_url(__FILE__)); ?>view/icons/back.svg" /> Retour
-						</button>
-						<section class="content-tab" id="content1"></section>
-				</div>
-			</div>
+		function wporg_custom_box_html($post) {
+			?>
+            <div id="ha-popup" class="ha-overlay">
+                <div class="ha-popup">
+                    <main>
+                        <a class="close" href="#">&times;</a>
+                        <button type="button" class="ha-btn ha-btn-secondary ha-return" style="display: none;"
+                                onclick="haReturn()">
+                            <img src="<?= esc_url(plugin_dir_url(__FILE__)); ?>view/icons/back.svg"/> Retour
+                        </button>
+                        <section class="content-tab" id="content1"></section>
+                </div>
+            </div>
 			<?php
-			$campaign = get_option('ha-campaign');
-			$nbCampaign = count($campaign);
-
 			if (get_option('ha-slug') == '') {
 				$pageWidget = "ha-no-sync";
 				$type = "error_1";
 			} else {
+				$campaign = get_option('ha-campaign');
+				$nbCampaign = count($campaign);
 				if (($nbCampaign == 0 or $campaign == '') && get_option('ha-error') == 0) {
 					$pageWidget = "ha-no-campaign";
 					$type = "error_2";
@@ -323,39 +311,52 @@ class Hello_Asso_Admin
 				}
 			}
 			?>
-			<a href="#ha-popup" id="ha-popup-open" onclick="loadViewCampaign('<?= esc_url(admin_url()); ?>admin.php?page=<?= esc_html($pageWidget); ?>&from=tinymce', '<?= esc_html($type); ?>')">Charger mes campagnes</a>
+            <a href="#ha-popup" id="ha-popup-open"
+               onclick="loadViewCampaign('<?= esc_url(admin_url()); ?>admin.php?page=<?= esc_html($pageWidget); ?>&from=tinymce', '<?= esc_html($type); ?>')">Charger
+                mes campagnes</a>
 
-		<?php
+			<?php
 		}
 
 
 		add_action('media_buttons', function ($editor_id) { ?>
-			<span style="display: inline-block">
+            <span style="display: inline-block">
 				<div class="ha-dropdown">
 					<a href="#" onclick="ha_dropdown()" class="ha-dropbtn ha-open-dropdown">
 						<span class="ha-open-dropdown" style="margin-right: 8px;">Code court</span>
-						<svg xmlns="http://www.w3.org/2000/svg" class="ha-open-dropdown" viewBox="0 0 200 43.5" style="width: 80px;">
+						<svg xmlns="http://www.w3.org/2000/svg" class="ha-open-dropdown" viewBox="0 0 200 43.5"
+                             style="width: 80px;">
 							<style>
 								.st38 {
-									fill: #fff;
-								}
+                                    fill: #fff;
+                                }
 							</style>
-							<path d="M71.1 19.3v13.3h-6.6v-12c0-1.4-.4-1.8-1-1.8-.7 0-1.5.6-2.2 1.8v12h-6.6v-25l6.6-.7v9.4c1.5-1.6 3-2.3 5-2.3 3-.1 4.8 1.9 4.8 5.3zM90.3 25.5H79.6c.4 2.6 1.6 3 3.6 3 1.3 0 2.5-.5 4-1.6l2.7 3.7c-2 1.7-4.7 2.7-7.3 2.7-6.5 0-9.6-4.1-9.6-9.6 0-5.3 3-9.7 8.9-9.7 5.2 0 8.7 3.4 8.7 9.4-.1.5-.2 1.4-.3 2.1zm-6.3-4c0-1.8-.4-3.3-2.1-3.3-1.4 0-2.1.8-2.4 3.6H84v-.3zM92.1 27.4V7.5l6.6-.7v20.3c0 .6.3.9.8.9.2 0 .5 0 .7-.2l1.2 4.7c-1.2.4-2.4.6-3.6.6-3.7.2-5.7-2-5.7-5.7zM102.1 27.4V7.5l6.6-.7v20.3c0 .6.3.9.8.9.2 0 .5 0 .7-.2l1.2 4.7c-1.2.4-2.4.6-3.6.6-3.7.2-5.7-2-5.7-5.7zM129.4 23.6c0 5.9-3.5 9.6-9.2 9.6-5.6 0-9.2-3.4-9.2-9.7 0-5.9 3.5-9.6 9.2-9.6 5.6 0 9.2 3.5 9.2 9.7zm-11.5 0c0 3.6.7 4.9 2.4 4.9 1.6 0 2.4-1.4 2.4-4.9 0-3.6-.7-4.9-2.4-4.9s-2.5 1.5-2.4 4.9zM147.8 28.9l-1.3 4.3c-2.3-.2-3.8-.8-4.8-2.5-1.3 2-3.3 2.6-5.4 2.6-3.6 0-5.9-2.4-5.9-5.7 0-4 3-6.2 8.6-6.2h1.3v-.5c0-1.8-.6-2.3-2.6-2.3-1.6.1-3.1.4-4.6.9l-1.4-4.2c2.2-.9 4.6-1.4 7-1.4 5.7 0 8 2.2 8 6.6v6.2c0 1.3.3 1.9 1.1 2.2zm-7.5-1.4v-2.7h-.7c-1.9 0-2.7.6-2.7 2 0 1 .6 1.7 1.5 1.7.7.1 1.5-.3 1.9-1zM163.6 16.3l-2.3 3.6c-1.3-.8-2.7-1.3-4.2-1.3-1.1 0-1.5.3-1.5.8 0 .6.2.9 3.6 1.9 3.4 1.1 5.2 2.5 5.2 5.8 0 3.7-3.5 6.2-8.4 6.2-3.1 0-6-1.1-7.8-2.9l3.1-3.5c1.3 1 2.9 1.8 4.5 1.8 1.2 0 1.9-.4 1.9-1.1 0-.9-.4-1.1-3.4-2-3.3-1-5.2-2.9-5.2-5.8 0-3.2 2.8-5.8 7.7-5.8 2.6-.1 5.2.8 6.8 2.3zM180.1 16.3l-2.3 3.6c-1.3-.8-2.7-1.3-4.2-1.3-1.1 0-1.5.3-1.5.8 0 .6.2.9 3.6 1.9 3.4 1.1 5.2 2.5 5.2 5.8 0 3.7-3.5 6.2-8.4 6.2-3.1 0-6-1.1-7.8-2.9l3.1-3.5c1.3 1 2.9 1.8 4.5 1.8 1.2 0 1.9-.4 1.9-1.1 0-.9-.4-1.1-3.4-2-3.3-1-5.2-2.9-5.2-5.8 0-3.2 2.8-5.8 7.7-5.8 2.6-.1 5.1.8 6.8 2.3zM200 23.6c0 5.9-3.5 9.6-9.2 9.6-5.6 0-9.2-3.4-9.2-9.7 0-5.9 3.5-9.6 9.2-9.6 5.6 0 9.2 3.5 9.2 9.7zm-11.5 0c0 3.6.7 4.9 2.4 4.9 1.6 0 2.4-1.4 2.4-4.9 0-3.6-.7-4.9-2.4-4.9s-2.5 1.5-2.4 4.9z" class="st38" />
-							<linearGradient id="SVGID_1_" x1="4.322" x2="24.268" y1="33.651" y2="-.503" gradientTransform="matrix(1 0 0 -1 0 44.736)" gradientUnits="userSpaceOnUse">
-								<stop offset="0" stop-color="#498a63" />
-								<stop offset=".25" stop-color="#61b984" />
+							<path d="M71.1 19.3v13.3h-6.6v-12c0-1.4-.4-1.8-1-1.8-.7 0-1.5.6-2.2 1.8v12h-6.6v-25l6.6-.7v9.4c1.5-1.6 3-2.3 5-2.3 3-.1 4.8 1.9 4.8 5.3zM90.3 25.5H79.6c.4 2.6 1.6 3 3.6 3 1.3 0 2.5-.5 4-1.6l2.7 3.7c-2 1.7-4.7 2.7-7.3 2.7-6.5 0-9.6-4.1-9.6-9.6 0-5.3 3-9.7 8.9-9.7 5.2 0 8.7 3.4 8.7 9.4-.1.5-.2 1.4-.3 2.1zm-6.3-4c0-1.8-.4-3.3-2.1-3.3-1.4 0-2.1.8-2.4 3.6H84v-.3zM92.1 27.4V7.5l6.6-.7v20.3c0 .6.3.9.8.9.2 0 .5 0 .7-.2l1.2 4.7c-1.2.4-2.4.6-3.6.6-3.7.2-5.7-2-5.7-5.7zM102.1 27.4V7.5l6.6-.7v20.3c0 .6.3.9.8.9.2 0 .5 0 .7-.2l1.2 4.7c-1.2.4-2.4.6-3.6.6-3.7.2-5.7-2-5.7-5.7zM129.4 23.6c0 5.9-3.5 9.6-9.2 9.6-5.6 0-9.2-3.4-9.2-9.7 0-5.9 3.5-9.6 9.2-9.6 5.6 0 9.2 3.5 9.2 9.7zm-11.5 0c0 3.6.7 4.9 2.4 4.9 1.6 0 2.4-1.4 2.4-4.9 0-3.6-.7-4.9-2.4-4.9s-2.5 1.5-2.4 4.9zM147.8 28.9l-1.3 4.3c-2.3-.2-3.8-.8-4.8-2.5-1.3 2-3.3 2.6-5.4 2.6-3.6 0-5.9-2.4-5.9-5.7 0-4 3-6.2 8.6-6.2h1.3v-.5c0-1.8-.6-2.3-2.6-2.3-1.6.1-3.1.4-4.6.9l-1.4-4.2c2.2-.9 4.6-1.4 7-1.4 5.7 0 8 2.2 8 6.6v6.2c0 1.3.3 1.9 1.1 2.2zm-7.5-1.4v-2.7h-.7c-1.9 0-2.7.6-2.7 2 0 1 .6 1.7 1.5 1.7.7.1 1.5-.3 1.9-1zM163.6 16.3l-2.3 3.6c-1.3-.8-2.7-1.3-4.2-1.3-1.1 0-1.5.3-1.5.8 0 .6.2.9 3.6 1.9 3.4 1.1 5.2 2.5 5.2 5.8 0 3.7-3.5 6.2-8.4 6.2-3.1 0-6-1.1-7.8-2.9l3.1-3.5c1.3 1 2.9 1.8 4.5 1.8 1.2 0 1.9-.4 1.9-1.1 0-.9-.4-1.1-3.4-2-3.3-1-5.2-2.9-5.2-5.8 0-3.2 2.8-5.8 7.7-5.8 2.6-.1 5.2.8 6.8 2.3zM180.1 16.3l-2.3 3.6c-1.3-.8-2.7-1.3-4.2-1.3-1.1 0-1.5.3-1.5.8 0 .6.2.9 3.6 1.9 3.4 1.1 5.2 2.5 5.2 5.8 0 3.7-3.5 6.2-8.4 6.2-3.1 0-6-1.1-7.8-2.9l3.1-3.5c1.3 1 2.9 1.8 4.5 1.8 1.2 0 1.9-.4 1.9-1.1 0-.9-.4-1.1-3.4-2-3.3-1-5.2-2.9-5.2-5.8 0-3.2 2.8-5.8 7.7-5.8 2.6-.1 5.1.8 6.8 2.3zM200 23.6c0 5.9-3.5 9.6-9.2 9.6-5.6 0-9.2-3.4-9.2-9.7 0-5.9 3.5-9.6 9.2-9.6 5.6 0 9.2 3.5 9.2 9.7zm-11.5 0c0 3.6.7 4.9 2.4 4.9 1.6 0 2.4-1.4 2.4-4.9 0-3.6-.7-4.9-2.4-4.9s-2.5 1.5-2.4 4.9z"
+                                  class="st38"/>
+							<linearGradient id="SVGID_1_" x1="4.322" x2="24.268" y1="33.651" y2="-.503"
+                                            gradientTransform="matrix(1 0 0 -1 0 44.736)"
+                                            gradientUnits="userSpaceOnUse">
+								<stop offset="0" stop-color="#498a63"/>
+								<stop offset=".25" stop-color="#61b984"/>
 							</linearGradient>
-							<path fill="url(#SVGID_1_)" d="M12.9 34.9c-6.6-7.6-2.2-26.8.6-26.8C8.1 7.9-1.1 11.5.2 24.4c1.5 12 12.3 20.4 24.1 18.9 3.8-.5 7.3-2 10.3-4.3-10.4 7.5-17.4.8-21.7-4.1z" />
-							<linearGradient id="SVGID_2_" x1="19.889" x2="40.524" y1="3.627" y2="36.697" gradientTransform="matrix(1 0 0 -1 0 44.736)" gradientUnits="userSpaceOnUse">
-								<stop offset="0" stop-color="#89356d" />
-								<stop offset=".21" stop-color="#b94794" />
+							<path fill="url(#SVGID_1_)"
+                                  d="M12.9 34.9c-6.6-7.6-2.2-26.8.6-26.8C8.1 7.9-1.1 11.5.2 24.4c1.5 12 12.3 20.4 24.1 18.9 3.8-.5 7.3-2 10.3-4.3-10.4 7.5-17.4.8-21.7-4.1z"/>
+							<linearGradient id="SVGID_2_" x1="19.889" x2="40.524" y1="3.627" y2="36.697"
+                                            gradientTransform="matrix(1 0 0 -1 0 44.736)"
+                                            gradientUnits="userSpaceOnUse">
+								<stop offset="0" stop-color="#89356d"/>
+								<stop offset=".21" stop-color="#b94794"/>
 							</linearGradient>
-							<path fill="url(#SVGID_2_)" d="M37.2 21.9C31.7 33 14.8 37.7 12.9 34.8c3.3 4.9 11.5 11.6 21.8 4 9.4-7.3 11.1-21 3.8-30.5-2.3-3-5.4-5.3-8.9-6.8 11.7 5.3 10.5 14.6 7.6 20.4z" />
-							<linearGradient id="SVGID_3_" x1="3.242" x2="37.689" y1="35.782" y2="23.384" gradientTransform="matrix(1 0 0 -1 0 44.736)" gradientUnits="userSpaceOnUse">
-								<stop offset=".6" stop-color="#f59c1c" />
-								<stop offset="1" stop-color="#c7702b" />
+							<path fill="url(#SVGID_2_)"
+                                  d="M37.2 21.9C31.7 33 14.8 37.7 12.9 34.8c3.3 4.9 11.5 11.6 21.8 4 9.4-7.3 11.1-21 3.8-30.5-2.3-3-5.4-5.3-8.9-6.8 11.7 5.3 10.5 14.6 7.6 20.4z"/>
+							<linearGradient id="SVGID_3_" x1="3.242" x2="37.689" y1="35.782" y2="23.384"
+                                            gradientTransform="matrix(1 0 0 -1 0 44.736)"
+                                            gradientUnits="userSpaceOnUse">
+								<stop offset=".6" stop-color="#f59c1c"/>
+								<stop offset="1" stop-color="#c7702b"/>
 							</linearGradient>
-							<path fill="url(#SVGID_3_)" d="M13.5 8.1c11.9-1.3 25.4 11 23.7 13.9 3.3-5.8 4.1-15.1-7.5-20.4C18.6-2.9 6 2.5 1.6 13.7.2 17.2-.3 21 .2 24.7-.6 11.9 9.1 8.5 13.5 8.1z" />
+							<path fill="url(#SVGID_3_)"
+                                  d="M13.5 8.1c11.9-1.3 25.4 11 23.7 13.9 3.3-5.8 4.1-15.1-7.5-20.4C18.6-2.9 6 2.5 1.6 13.7.2 17.2-.3 21 .2 24.7-.6 11.9 9.1 8.5 13.5 8.1z"/>
 						</svg>
 					</a>
 					<div id="ha-dropdown" class="ha-dropdown-content">
@@ -380,21 +381,23 @@ class Hello_Asso_Admin
 							}
 						}
 						?>
-						<a href="#ha-popup" onclick="loadViewCampaign('<?= esc_url(admin_url()); ?>admin.php?page=<?= esc_html($pageWidget); ?>&from=tinymce', '<?= esc_html($type); ?>')">Charger mes campagnes</a>
+						<a href="#ha-popup"
+                           onclick="loadViewCampaign('<?= esc_url(admin_url()); ?>admin.php?page=<?= esc_html($pageWidget); ?>&from=tinymce', '<?= esc_html($type); ?>')">Charger mes campagnes</a>
 					</div>
 				</div>
 			</span>
-			<div id="ha-popup" class="ha-overlay">
-				<div class="ha-popup">
-					<main>
-						<a class="close" href="#">&times;</a>
-						<button type="button" class="ha-btn ha-btn-secondary ha-return" style="display: none;" onclick="haReturn()">
-							<img src="<?= esc_url(plugin_dir_url(__FILE__)); ?>view/icons/back.svg" /> Retour
-						</button>
-						<section class="content-tab" id="content1"></section>
-				</div>
-			</div>
-<?php
+            <div id="ha-popup" class="ha-overlay">
+                <div class="ha-popup">
+                    <main>
+                        <a class="close" href="#">&times;</a>
+                        <button type="button" class="ha-btn ha-btn-secondary ha-return" style="display: none;"
+                                onclick="haReturn()">
+                            <img src="<?= esc_url(plugin_dir_url(__FILE__)); ?>view/icons/back.svg"/> Retour
+                        </button>
+                        <section class="content-tab" id="content1"></section>
+                </div>
+            </div>
+			<?php
 		});
 	}
 
@@ -403,8 +406,7 @@ class Hello_Asso_Admin
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_scripts()
-	{
+	public function enqueue_scripts() {
 
 		/**
 		 * This function is provided for demonstration purposes only.
@@ -432,16 +434,12 @@ class Hello_Asso_Admin
 	}
 
 
-
-
-	public function loadAjax()
-	{
+	public function loadAjax() {
 
 		add_action('wp_ajax_ha_ajax', 'ha_ajax');
 		add_action('wp_ajax_nopriv_ha_ajax', 'ha_ajax');
 
-		function sanitizeArray($data = array())
-		{
+		function sanitizeArray($data = array()) {
 			if (!is_array($data) || !count($data)) {
 				return array();
 			}
@@ -455,8 +453,8 @@ class Hello_Asso_Admin
 			}
 			return $data;
 		}
-		function ha_ajax()
-		{
+
+		function ha_ajax() {
 			check_ajax_referer('helloassosecuritytoken11', 'security');
 
 			if (!isset($_POST['campaign']) or $_POST['campaign'] == '') {
@@ -481,7 +479,7 @@ class Hello_Asso_Admin
 				add_option('ha-sync', current_time('timestamp'), '', 'yes');
 				add_option('ha-error', $error, '', 'yes');
 				add_option('ha-name', $name, '', 'yes');
-			} elseif(isset($_POST['slug']) && is_numeric($_POST['error']) && is_array($campaign) && isset($_POST['increase']) && $_POST['increase'] > 1) {
+			} elseif (isset($_POST['slug']) && is_numeric($_POST['error']) && is_array($campaign) && isset($_POST['increase']) && $_POST['increase'] > 1) {
 				$currentCampain = get_option('ha-campaign');
 				// increade current campain with campain
 				$campaign = array_merge($currentCampain, $campaign);
